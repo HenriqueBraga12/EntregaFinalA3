0af8339875870545e6732ae8b276b9a3f8a868f5
Braga <127881212+HenriqueBraga12@users.noreply.github.com>
2023-11-11 22:39:56 -0300

    Criando funções de paginação

--- Js/index.js
+++ Js/index.js
@@ -1,4 +1,17 @@
+//Alert -- Manutenção
 function mensagem() {
 	console.log("mensagem disparada")
 	return alert("Aguarde! Estamos trabalhando");
 }
+
+//Redirecionando para pg login
+function login () {
+	console.log("enviando usuario para login")
+	window.location.href='../html/login.html'
+}
+
+//Redirecionando para cadastro
+function membro(){
+	window.location.href='../html/cadastro.html'
+}
+
