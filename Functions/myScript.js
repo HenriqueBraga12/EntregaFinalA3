478822f7f88ffd2f6aeddffe80f9a2e07ca303dd
JailsonTech <jailsonrodriguestech@gmail.com>
2023-11-13 15:51:35 -0300

    dropdown do +jogos

--- Functions/myScript.js
+++ Functions/myScript.js
@@ -1,5 +1,5 @@
 
-
+// ...DROPDOWN DO MOBILE...............
 function myFunction() {
  document.getElementById("myDropdown").classList.toggle("mostrar");
 }
@@ -12,7 +12,19 @@ window.onclick = function(e) {
    }
  }
 }
-
+// ...AVISO DE TRABALHANDO...............
 function aviso(){
  alert("Aguarde!\n Estamos trabalhando"); 
 }
+// ...DROPDOWN DE +JOGOS...............
+function jogos(){
+  document.getElementById("maisJogos").classList.toggle("mostrar");
+}
+window.onclick = function(e) {
+  if (!e.target.matches('.boxJogos')) {
+  var dropdownJogos = document.getElementById("dropdownJogos");
+    if (dropdownJogos.classList.contains('mostrar')) {
+      dropdownJogos.classList.remove('mostrar');
+    }
+  }
+ }
