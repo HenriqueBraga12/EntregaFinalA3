ecaeac608e9383e1130c7a423a696044c517fdf5
Braga <127881212+HenriqueBraga12@users.noreply.github.com>
2023-12-07 11:42:32 -0300

    Adicionando page CreateGame

--- src/web/src/pages/CreateGame/CreateGame.js
+++ src/web/src/pages/CreateGame/CreateGame.js
@@ -1,7 +1,17 @@
-import { Logo } from '../../components'
+import { Logo, Input } from '../../components'
 
-export default function CreateGame (){
+export default function CreateGame() {
 	return (
-		
+		<div className='library'>
+			<div className='library-container'>
+				<Logo />
+			</div>
+			<div className='form-container'>
+				<form action='' method='' className='form-style'>
+					<Input name='game_id' label='Escolha um jogo'/>
+				</form>
+			</div>
+		</div>
 	)
+
 }
