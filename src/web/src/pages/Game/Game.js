f26b681794c5c8761b559c7031707642ddeb1345
JailsonTech <jailsonrodriguestech@gmail.com>
2023-12-11 21:43:46 -0300

    adiconado no Signup - toast.error(inválido! use 8 caracteres)

--- src/web/src/pages/Game/Game.js
+++ src/web/src/pages/Game/Game.js
@@ -86,8 +86,7 @@ export default function Game() {
                 <h3 className="game-info__item__value">{game.developer}</h3>
               </div>
             )}
-
-            {game?.grade && game.grade > 0 && (
+            {game?.grade && (
               <div className="game-info__item">
                 <span className="game-info__item__label">Sua avaliação</span>
                 <Rating value={game.grade} readOnly={true} />
